30e1d9713f3c1bc7048fb764abb4275ba019935c
Djemara <djemaraamicy02@gmail.com>
2026-08-10 08:41:20 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const path = require('path');
+
 
 
 const authRoutes = require('./routes/authRoutes');
@@ -16,10 +16,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '../frontend')));
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/index.html'));
-});
+
 
 
 
