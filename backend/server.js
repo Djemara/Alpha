8ef18e7d3d8f3998bf9f49b90847bfada668fc77
Djemara <djemaraamicy02@gmail.com>
2026-08-10 07:55:07 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const path = require('path');
+
 
 const authRoutes = require('./routes/authRoutes');
 const contactRoutes = require('./routes/contactRoutes');
